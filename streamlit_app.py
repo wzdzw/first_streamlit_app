e48a58e15a45486c9d10d4b5fc34d74cff2eb994
wzdzw <53460773+wzdzw@users.noreply.github.com>
2022-06-08 10:38:50 +0930

    Update streamlit_app.py
    Add pandas dataframe for fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,6 @@ st.text('🐔 Hard-boiled free range egg')
 st.text('🥑🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
