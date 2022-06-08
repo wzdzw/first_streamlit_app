c2d64d62ec94415951e2a1eeb76c228c9f8e9328
wzdzw <53460773+wzdzw@users.noreply.github.com>
2022-06-08 10:45:28 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,10 @@ st.text('🐔 Hard-boiled free range egg')
 st.text('🥑🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# put a pick list here so they can pick the fruit they want to include
+st.multiselect("pick some fruits:",list(my_fruit_list.index))
+
+# display fruit table on the page
 st.dataframe(my_fruit_list)
